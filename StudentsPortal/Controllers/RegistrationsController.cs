7222e67c3791dbf9a8ee7fce9ea9bdd11bb96b2c
uduji chikordi <udujichikordi@gmail.com>
2019-07-01 16:21:20 +0100

    first and long commit

---
+++ StudentsPortal/Controllers/RegistrationsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StudentsPortal.Models.StudentsDetails;
+
+namespace StudentsPortal.Controllers
+{
+    public class RegistrationsController : Controller
+    {
+        // GET: Registrations
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DashBoard()
+        {
+            return View();
+        }
+
+        public ActionResult RegisterCourses()
+        {
+            return View();
+        }
+    }
+}
---
+++ StudentsPortal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(StudentsPortal.Startup))]
+namespace StudentsPortal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
